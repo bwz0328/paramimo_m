5cdcbe1a08fb9bf0773e11b520aa0e683e5ccfc2
bwz0328 <bwz0328@126.com>
2018-10-03 21:11:45 +0800

    run test

--- transport.py
+++ transport.py
@@ -1980,7 +1980,7 @@ class Transport(threading.Thread, ClosingContextManager):
                 self._expect_packet(MSG_KEXINIT)
 
                 while self.active:
-                    print("run loop1.", threading.currentThread().getName())
+                    print("run loop2.", threading.currentThread().getName())
                     if self.packetizer.need_rekey() and not self.in_kex:
                         print("run  1!")
                         self._send_kex_init()
