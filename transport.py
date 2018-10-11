ac750ff20188515a09812bd10e895e0f227afdc4
bwz0328 <bwz0328@126.com>
2018-10-11 16:09:26 +0800

    create run_with_noblocking

--- transport.py
+++ transport.py
@@ -409,6 +409,11 @@ class Transport(threading.Thread, ClosingContextManager):
         threading.Thread.__init__(self)
         self.setDaemon(True)
         self.sock = sock
+        #add by bwz
+        self.threadM = threadm_forSSH()
+        self.threadM.add_sock(self)
+        #maybe can not recyle instance?????
+        #add by bwz end
         # we set the timeout so we can check self.active periodically to
         # see if we should bail. socket.timeout exception is never propagated.
         self.sock.settimeout(self._active_check_timeout)
@@ -2710,7 +2715,7 @@ class Transport(threading.Thread, ClosingContextManager):
                         ptype, m = self.packetizer.read_message()
                         cmdname = str(ptype)
                         if ptype in MSG_NAMES:
-                            cmdname = MSG_NAMES[ptype]
+                            cmdname = MSG_NAMES[ptype] + "[" + ptype + "]"
                         print("    [run_for_noblocking] ==> get msg, type[" , cmdname , "] Data=>:")
                     except NeedRekeyException:
                         break
@@ -3826,3 +3831,75 @@ class ChannelMap(object):
             return len(self._map)
         finally:
             self._lock.release()
+
+
+
+
+
+import select
+import threading
+
+
+def singleton(cls):  
+        instances = {}  
+        def getinstance():  
+            if cls not in instances:  
+                instances[cls] = cls()  
+            return instances[cls]  
+        return getinstance  
+     
+@singleton    
+class MyEpoll_forSSH:
+    __fd_to_socket_info = {}
+    __epoll = None
+    def __new__(self):
+        print("new:", self)
+    def __init__(self):
+        print("init:", self)
+        self.__epoll = select.epoll()
+    def add_socket(self, sockid , info = None):
+        self.__epoll.register(sockid.fileno(), select.EPOLLIN)
+        self.__fd_to_socket_info[sockid.fileno()] = {"socket":sockid, "info":info }
+    def my_wait(self, timeout = 1):
+        event_ret = {}
+        events = self.__epoll.poll(timeout)
+        if not events:
+            return None;
+        for fd, event in events:
+           if event & select.EPOLLIN:
+               event_ret[fd] = self.__fd_to_socket_info[fd]
+        return event_ret
+
+
+
+@singleton   
+class threadm_forSSH(threading.Thread):
+   def __init__(self, timeout = 1):
+       threading.Thread.__init__(self)
+       self.mypoll = MyEpoll_forSSH()
+       self.connectList = []
+       self.cuCount = 0
+       self.timeout = timeout
+       self.start()
+   def run(self):
+        while True:
+            evs = self.mypoll.my_wait(self.timeout)
+            if evs is None:
+               print("Timeout 10")
+            else:
+               print(evs)
+               for fd in evs:
+                   print("sock [%s]===> %s"  %(evs[fd]["info"]["msg"], "***"))
+                   for conn in self.connectList:
+                       conn.run_for_noblocking()
+
+                   
+   def add_sock(self, transport):
+       self.connectList.append(transport)
+       self.mypoll.add_socket(transport.sock, {"transp":transport, "msg":"here is " + str(self.cuCount)})
+       self.cuCount = self.cuCount + 1
+
+
+
+
+
